8fff31f1d94128ddcdd4f1d65e24f3969c57f934
zseen <43151120+zseen@users.noreply.github.com>
2020-12-03 20:54:58 +0000

    Add class StepsInDirections

--- 2020/03/Day3-TobogganTrajectory.py
+++ 2020/03/Day3-TobogganTrajectory.py
@@ -8,90 +8,78 @@ class MapObject(enum.Enum):
     tree = "#"
     square = "."
 
+
+class StepsInDirections:
+    def __init__(self, rightSteps, downSteps):
+        self.rightSteps = rightSteps
+        self.downSteps = downSteps
+
+
 class Map:
-    def __init__(self, mostRightSteps, mostDownSteps):
-        self.mostRightSteps = mostRightSteps
-        self.mostDownSteps = mostDownSteps
-        self.originalWidth = None
-        self.originalLength = None
-        self.originalMap = []
+    def __init__(self):
+        self.originalMap = self.getMap()
         self.repeatedMap = []
 
-
     def getMap(self):
+        originalMap = []
         with open(INPUT_FILE, "r") as inputFile:
             lines = inputFile.readlines()
             for line in lines:
-                self.originalMap.append(line.strip('\n'))
+                originalMap.append(line.strip('\n'))
+        return originalMap
 
-
-    def getRepeatedMap(self):
-        self.getMap()
+    def getRepeatedMap(self, stepsInDirections: StepsInDirections):
+        self.repeatedMap = []
         if not self.originalMap:
             raise ValueError("Map not found")
-        self.originalWidth = len(self.originalMap[0])
-        self.originalLength = len(self.originalMap)
 
-        canGoInOneOriginalMap = self.originalWidth // self.mostRightSteps
-        repeatNeeded = self.originalLength // canGoInOneOriginalMap + 1
+        originalWidth = len(self.originalMap[0])
+        rowsCount = len(self.originalMap)
+
+        rightMovemenInOriginalMapCount = originalWidth // stepsInDirections.rightSteps
+        repeatNeeded = (rowsCount // rightMovemenInOriginalMapCount // stepsInDirections.downSteps) + 1
 
         for row in self.originalMap:
             self.repeatedMap.append(row * repeatNeeded)
+
         return self.repeatedMap
 
 
-def countTrees(map: Map):
+def countTrees(map: Map, stepsInDirections: StepsInDirections):
     treeCount = 0
     currentRow = 0
     currentColumn = 0
-    # print(map.repeatedMap)
-    # #print(map.repeatedMap[currentRow][currentColumn])
-    # print(map.repeatedMap[currentRow+1][currentColumn+3])
-    # print(map.repeatedMap[currentRow + 2][currentColumn + 6])
-    # print(map.repeatedMap[currentRow + 3][currentColumn + 9])
-    # print(map.repeatedMap[currentRow + 4][currentColumn + 12])
-    # print(map.repeatedMap[currentRow + 5][currentColumn + 15])
-    # print(map.repeatedMap[currentRow + 6][currentColumn + 18])
-    # print(map.repeatedMap[currentRow + 7][currentColumn + 21])
-    # print(map.repeatedMap[currentRow + 8][currentColumn + 24])
-    # print(map.repeatedMap[currentRow + 9][currentColumn + 27])
-    # print(map.repeatedMap[currentRow + 10][currentColumn + 30])
+    currentMap = map.getRepeatedMap(stepsInDirections)
+    bottomIndex = len(map.repeatedMap) - 1
 
-    while currentRow != map.originalLength - 1:
-        currentRow += map.mostDownSteps
-        currentColumn += map.mostRightSteps
-        currentCell = map.repeatedMap[currentRow][currentColumn]
+    while currentRow != bottomIndex:
+        currentRow += stepsInDirections.downSteps
+        currentColumn += stepsInDirections.rightSteps
+        currentCell = currentMap[currentRow][currentColumn]
         if currentCell == MapObject.tree.value:
             treeCount += 1
 
     return treeCount
 
 
+def getTreesCountWithDifferentDirections(map, allStepsInDirections):
+    allTreesCombined = 1
+    for stepsInDirections in allStepsInDirections:
+        allTreesCombined *= countTrees(map, stepsInDirections)
+
+    return allTreesCombined
 
 
 def main():
-    multipliedTrees = 1
-    map1 = Map(1, 1)
-    map1.getRepeatedMap()
-    multipliedTrees *= countTrees(map1)
+    map = Map()
+    allWalkMethodsDirections = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    allStepsInDirections = []
+    for walkMethodDirection in allWalkMethodsDirections:
+        allStepsInDirections.append(StepsInDirections(walkMethodDirection[0], walkMethodDirection[1]))
 
-    map2 = Map(3, 1)
-    map2.getRepeatedMap()
-    multipliedTrees *= (countTrees(map2))
+    print(countTrees(map, StepsInDirections(3, 1)))  # 270
+    print(getTreesCountWithDifferentDirections(map, allStepsInDirections))  # 2122848000
 
-    map3 = Map(5, 1)
-    map3.getRepeatedMap()
-    multipliedTrees *= (countTrees(map3))
-
-    map4 = Map(7, 1)
-    map4.getRepeatedMap()
-    multipliedTrees *= (countTrees(map4))
-
-    map5 = Map(1, 2)
-    map5.getRepeatedMap()
-    multipliedTrees *= (countTrees(map5))
-
-    print(multipliedTrees)
 
 if __name__ == '__main__':
     main()
