fe3df18a77ec449ad7f15470729aac1f0060ce44
zseen <43151120+zseen@users.noreply.github.com>
2020-12-04 22:26:50 +0000

    Add unittests to both parts of the exercise

--- 2020/04/Day4-PassportProcessing.py
+++ 2020/04/Day4-PassportProcessing.py
@@ -3,96 +3,87 @@ import re
 from typing import List
 
 INPUT_FILE = "input.txt"
-TEST_INPUT_FILE = "test_input.txt"
+TEST_INPUT_FILE_ONE = "test_input_1.txt"
+TEST_INPUT_FILE_TWO = "test_input_2.txt"
 
-PASSPORT_FIELDS = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"}
-PASSPORT_FIELDS_TO_ACCEPTABLE_VALUES = {"byr": '[1][9][2-9][0-9]|[2][0][0][1-2]', "iyr": '[2][0][1][0-9]|[2][0][2][0]',
-                                        "eyr": '[2][0][2][0-9]|[2][0][3][0]',
-                                        "hgt": '[1][5-8][0-9][c][m]|[1][9][0-3][c][m]|[5][9][i][n]|[6][0-9][i][n]|[7][0-6][i][n]',
-                                        "hcl": '[#][0-9a-f]{6}', "ecl": '(amb)|(blu)|(brn)|(gry)|(grn)|(hzl)|(oth)',
-                                        "pid": '[0-9]{9}$', "cid": '\s|.'}
+PASSPORT_FIELDS_TO_ACCEPTABLE_REGEX_VALUES = {"byr": '[1][9][2-9][0-9]|[2][0][0][1-2]',
+                                              "iyr": '[2][0][1][0-9]|[2][0][2][0]',
+                                              "eyr": '[2][0][2][0-9]|[2][0][3][0]',
+                                              "hgt": '[1][5-8][0-9][c][m]|[1][9][0-3][c][m]|[5][9][i][n]|[6][0-9][i][n]|[7][0-6][i][n]',
+                                              "hcl": '[#][0-9a-f]{6}',
+                                              "ecl": '(amb)|(blu)|(brn)|(gry)|(grn)|(hzl)|(oth)',
+                                              "pid": '[0-9]{9}$', "cid": '\s|.'}
 
-
-P_F_T_A_V =  {"byr": (1920, 2002), "iyr": (2010, 2020),
-            "eyr": (2020, 2030),
-            "hgt": [(150, 193), (59, 76)],
-            "hcl": ["#", 0, 9, "a", "f"],
-            "ecl": ["amb", "blu","brn", "gry","grn","hzl","oth"],
-            "pid": (0, 9), "cid": [True]}
 COUNTRY_ID_FIELD = "cid"
 
 
-def getInput():
-    inputList = [[]]
+def getInput(inputFile):
+    dataForAllPassports = [[]]
 
-    with open(INPUT_FILE, "r") as inputFile:
+    with open(inputFile, "r") as inputFile:
         lines = inputFile.readlines()
         for line in lines:
             if line == "\n":
-                inputList.append([])
+                dataForAllPassports.append([])
             else:
                 line = line.strip("\n")
-                lineSplit = re.split('-|: | ', line)
-                inputList[-1].extend(lineSplit)
+                lineSplit = re.split(' ', line)
+                dataForAllPassports[-1].extend(lineSplit)
 
-    fieldToDataAllPassports = getFieldToData(inputList)
-    return fieldToDataAllPassports
-
-
-def getFieldToData(passportDataList):
-    fieldToDataList = []
-    for passport in passportDataList:
-        passportFieldToDataCollection = dict()
-        for entry in passport:
-            entry = entry.split(':')
-            passportFieldToDataCollection[str(entry[0])] = str(entry[1])
-        fieldToDataList.append(passportFieldToDataCollection)
-    return fieldToDataList
+    allPassportsWithFieldToData: List = getAllPassportsWithFieldToData(dataForAllPassports)
+    return allPassportsWithFieldToData
 
 
-def isPassportValidInitialCheck(passport: dict):
+def createPassportWithFieldToData(rawPassport: List):
+    passportAllFieldsToData = dict()
+    for entry in rawPassport:
+        entry = entry.split(':')
+        passportAllFieldsToData[str(entry[0])] = str(entry[1])
+    return passportAllFieldsToData
+
+
+def getAllPassportsWithFieldToData(rawPassportsCollection: List):
+    allPassportsWithFieldToData: List = []
+    for rawPassport in rawPassportsCollection:
+        allPassportsWithFieldToData.append(createPassportWithFieldToData(rawPassport))
+    return allPassportsWithFieldToData
+
+
+def isPassportFormatValid(passport: dict):
     passportFields = set(passport.keys())
-    if passportFields == set(PASSPORT_FIELDS):
+    allPassportFields = set(PASSPORT_FIELDS_TO_ACCEPTABLE_REGEX_VALUES.keys())
+    if passportFields == allPassportFields:
         return True
-    return PASSPORT_FIELDS - passportFields == {COUNTRY_ID_FIELD}
-
-
-def isDataInFieldAcceptable(field, data):
-    pattern = PASSPORT_FIELDS_TO_ACCEPTABLE_VALUES[field]
-    match = re.match(pattern, data)
-    return match is not None
-
-
-
-
+    return allPassportFields - passportFields == {COUNTRY_ID_FIELD}
 
 
 def isPassportValid(passport: dict):
+    if not isPassportFormatValid(passport):
+        return False
+
     for field, data in passport.items():
-        # print(field, data)
-        if not isDataInFieldAcceptable(field, data):
+        if not isDataInFieldAcceptable(data, field):
             return False
     return True
 
 
-def getValidPassportsNum(passports: List):
+def isDataInFieldAcceptable(data, field) -> bool:
+    pattern = PASSPORT_FIELDS_TO_ACCEPTABLE_REGEX_VALUES[field]
+    match = re.match(pattern, data)
+    # match is either an object or None, the "is not None" part is only there to express that a boolean is returned in both cases (match found or not)
+    return match is not None
+
+
+def getValidPassportsCount(passports: List, validatingFunc):
     validPassportCounter = 0
     for passport in passports:
-        #print("passport:", passport)
-        if isPassportValidInitialCheck(passport) and isPassportValid(passport):
+        if validatingFunc(passport):
             validPassportCounter += 1
     return validPassportCounter
 
-#-------------------------
 
+# This function below will be deleted, just wanted to show this abomination
 def isDataInFieldAcceptableAlternative(field, data):
-
-    # P_F_T_A_V =  {"byr": (1920, 2002), "iyr": (2010, 2020),
-    #             "eyr": (2020, 2030),
-    #             "hgt": [(150, 193), (59, 76)],
-    #             "hcl": ["#", 0, 9, "a", "f"],
-    #             "ecl": ["amb", "blu","brn", "gry","grn","hzl","oth"],
-    #             "pid": (0, 9), "cid": [None, True]}
     if field == "byr":
         dataInt = int(data)
         if 1920 <= dataInt <= 2002:
@@ -106,7 +97,7 @@ def isDataInFieldAcceptableAlternative(field, data):
         if 2020 <= dataInt <= 2030:
             return True
     elif field == "hgt":
-        if data[-1] == "m" or data[-1] =="n":
+        if data[-1] == "m" or data[-1] == "n":
             if data[-1] == "m":
                 if 150 <= int(data[0:len(data) - 2]) <= 193:
                     return True
@@ -120,7 +111,6 @@ def isDataInFieldAcceptableAlternative(field, data):
         if data[0] != "#":
             return False
         poss = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
-
         for char in data[1:]:
             if char not in poss:
                 return False
@@ -139,35 +129,23 @@ def isDataInFieldAcceptableAlternative(field, data):
 
     return False
 
-def getValidPassportsNumALTERNAIVE(passports: List):
-    validPassportCounter = 0
-    for passport in passports:
-        #print("passport:", passport)
-        if isPassportValidInitialCheck(passport) and isPassportValidAlt(passport):
-            validPassportCounter += 1
-    return validPassportCounter
 
-def isPassportValidAlt(passport: dict):
-    for field, data in passport.items():
-        # print(field, data)
-        if not isDataInFieldAcceptableAlternative(field, data):
-            return False
-    return True
+def main():
+    passports = getInput(INPUT_FILE)
+    print(getValidPassportsCount(passports, isPassportFormatValid))  # 233
+    print(getValidPassportsCount(passports, isPassportValid))  # 111
 
 
+class ValidPassportCounterTest(unittest.TestCase):
+    def test_getValidPassportsCount_onlyFormatRequirementChecked_correctNumOfValidPassportsReturned(self):
+        passports = getInput(TEST_INPUT_FILE_ONE)
+        self.assertEqual(2, getValidPassportsCount(passports, isPassportFormatValid))
 
-# byr: [1][9][2-9][0-9]|[2][0][0][1-2]
-# iyr: [2][0][1][0-9]|[2][0][2][0]
-# eyr: [2][0][2][0-9]|[2][0][3][0]
-# hgt: [1][5-8][0-9][c][m]|[1][9][0-3][c][m]|[5][9][i][n]|[6][0-9][i][n]|[7][0-6][i][n]
-# hcl: [#][0-9a-f]{6}
-# ecl: (amb)|(blu)|(brn)|(gry)|(grn)|(hzl)|(oth)
-# pid: [0-9]{9}
+    def test_getValidPassportsCount_dataInFieldsChecked_correctNumOfValidPassportsReturned(self):
+        passports = getInput(TEST_INPUT_FILE_TWO)
+        self.assertEqual(4, getValidPassportsCount(passports, isPassportValid))
 
 
-
-l = getInput()
-# print(l)
-v = getValidPassportsNum(l)
-print(v)
-
+if __name__ == '__main__':
+    # main()
+    unittest.main()
