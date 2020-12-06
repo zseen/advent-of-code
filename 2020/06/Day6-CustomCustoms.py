c3d2bc3706e3e621d5642e0cac3e3f7797cb9afd
zseen <43151120+zseen@users.noreply.github.com>
2020-12-06 20:13:11 +0000

    Add the solution to the second part

--- 2020/06/Day6-CustomCustoms.py
+++ 2020/06/Day6-CustomCustoms.py
@@ -19,8 +19,26 @@ def getInput(inputFile):
                 neededChars = [char for char in lineSplit if char.isalpha()]
                 allGroupsAnswers[-1].extend(neededChars)
 
+    #print(allGroupsAnswers)
     return allGroupsAnswers
 
+def getInputSecondPart(inputFile):
+    allGroupsAnswers = [[]]
+
+    with open(inputFile, "r") as inputFile:
+        lines = inputFile.readlines()
+        for line in lines:
+            if line == "\n":
+                allGroupsAnswers.append([])
+            else:
+                line = line.strip("\n")
+                #lineSplit = re.split('', line)
+                #neededChars = [char for char in lineSplit if char.isalpha()]
+                #allGroupsAnswers[-1].extend(neededChars)
+                #print(line)
+                allGroupsAnswers[-1].append(line)
+    #print(allGroupsAnswers)
+    return allGroupsAnswers
 
 def countUniqueAnswersInGroup(groupAnswers: List):
     return len(set(groupAnswers))
@@ -32,5 +50,47 @@ def getUniqueAnswersCountInAllGroups(allGroups: List[List]):
     return allUniqueAnswersCount
 
 
-v = getInput(INPUT_FILE)
-print(getUniqueAnswersCountInAllGroups(v))
+def getAnswersCountEveryoneAgreedOn(allGroups: List[List]):
+    allAgreedAnswers = 0
+    for groupAnswer in allGroups:
+        if len(groupAnswer) == 1:
+            allAgreedAnswers += len(groupAnswer[0])
+        else:
+            firstStringInGroup = groupAnswer[0]
+            allStringInGroupNum = len(groupAnswer)
+            commonCharsNum = 0
+            for char in firstStringInGroup:
+                currCharCount = 0
+                for i in range(0, allStringInGroupNum):
+                    if char in groupAnswer[i]:
+                        currCharCount += 1
+                if currCharCount == allStringInGroupNum:
+                    commonCharsNum += 1
+            allAgreedAnswers += commonCharsNum
+
+
+
+    return allAgreedAnswers
+
+
+
+
+
+
+
+v = getInput(TEST_INPUT)
+#print(getUniqueAnswersCountInAllGroups(v)) # 6416
+
+b = getInputSecondPart(TEST_INPUT)
+c = getInputSecondPart(INPUT_FILE)
+print(b)
+#print(c)
+
+print(getAnswersCountEveryoneAgreedOn(b))
+print(getAnswersCountEveryoneAgreedOn(c)) # 3225 is too high 3128 too high
+
+#print(getAnswersCountEveryoneAgreedOn([c[0]]))
+print(getAnswersCountEveryoneAgreedOn([['e', 'e', 'e', 'e']])) #1
+print(getAnswersCountEveryoneAgreedOn([ ['pfyh', 'hyf', 'dhfy']])) #3
+print(getAnswersCountEveryoneAgreedOn([['dqbpwhoar', 'pqohgd']])) #5
+print(getAnswersCountEveryoneAgreedOn([['sakhxnu', 'cdowx', 'xlj', 'lnxh', 'njxhf']])) #1
