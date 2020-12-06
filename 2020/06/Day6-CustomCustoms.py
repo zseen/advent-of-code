b365bb39010e733b48411430cc62b6446b05c4b9
zseen <43151120+zseen@users.noreply.github.com>
2020-12-06 21:31:50 +0000

    Add unittest for both parts

--- 2020/06/Day6-CustomCustoms.py
+++ 2020/06/Day6-CustomCustoms.py
@@ -5,9 +5,9 @@ from typing import List
 INPUT_FILE = "input.txt"
 TEST_INPUT = "test_input.txt"
 
-def getInput(inputFile):
-    allGroupsAnswers = [[]]
 
+def getInputForFirstPart(inputFile):
+    allGroupsAnswers = [[]]
     with open(inputFile, "r") as inputFile:
         lines = inputFile.readlines()
         for line in lines:
@@ -15,16 +15,12 @@ def getInput(inputFile):
                 allGroupsAnswers.append([])
             else:
                 line = line.strip("\n")
-                lineSplit = re.split('', line)
-                neededChars = [char for char in lineSplit if char.isalpha()]
-                allGroupsAnswers[-1].extend(neededChars)
-
-    #print(allGroupsAnswers)
+                allGroupsAnswers[-1].extend(line)
     return allGroupsAnswers
 
-def getInputSecondPart(inputFile):
-    allGroupsAnswers = [[]]
 
+def getInputForSecondPart(inputFile):
+    allGroupsAnswers = [[]]
     with open(inputFile, "r") as inputFile:
         lines = inputFile.readlines()
         for line in lines:
@@ -32,83 +28,67 @@ def getInputSecondPart(inputFile):
                 allGroupsAnswers.append([])
             else:
                 line = line.strip("\n")
-                #lineSplit = re.split('', line)
-                #neededChars = [char for char in lineSplit if char.isalpha()]
-                #allGroupsAnswers[-1].extend(neededChars)
-                #print(line)
                 allGroupsAnswers[-1].append(line)
-    #print(allGroupsAnswers)
     return allGroupsAnswers
+
 
 def countUniqueAnswersInGroup(groupAnswers: List):
     return len(set(groupAnswers))
 
+
 def getUniqueAnswersCountInAllGroups(allGroups: List[List]):
-    allUniqueAnswersCount = 0
-    for groupAnswer in allGroups:
-        allUniqueAnswersCount += countUniqueAnswersInGroup(groupAnswer)
-    return allUniqueAnswersCount
+    return sum(countUniqueAnswersInGroup(group) for group in allGroups)
 
 
-def getAnswersEveryoneAgreedOnCount(allGroups: List[List]):
-    allAgreedAnswers = 0
-    for groupAnswer in allGroups:
-        shortestStringInGroupAnswer = min(groupAnswer, key=len)
-        commonCharsNum = 0
-        for char in shortestStringInGroupAnswer:
-            isCharCommonInAllStrings = True
-            for i in range(0, len(groupAnswer)):
-                if char not in groupAnswer[i]:
-                    isCharCommonInAllStrings = False
-                    break
-            if isCharCommonInAllStrings:
-                commonCharsNum += 1
-        allAgreedAnswers += commonCharsNum
-
-    return allAgreedAnswers
+def countCommonAnswersInGroup(group: List):
+    commonAnswer = set(group[0])
+    for i in range(1, len(group)):
+        currentAnswers = commonAnswer
+        nextAnswers = set(group[i])
+        commonAnswer = currentAnswers.intersection(nextAnswers)
+    return len(commonAnswer)
 
 
-def getAnswersEveryoneAgreedOnCount2(ga):
-    intersection = set(ga[0])
-    for i in range(1, len(ga)):
-        currentStringAsSet = intersection
-        nextStringAsSet = set(ga[i])
-        intersection = currentStringAsSet.intersection(nextStringAsSet)
-    return len(intersection)
+def countCommonGroupAnswersInAllGroups(allGroups: List[List]):
+    return sum(countCommonAnswersInGroup(group) for group in allGroups)
 
 
-def getAll(allGroups: List[List]):
-    allAg =  0
-    for groupAnswer in allGroups:
-        allAg += getAnswersEveryoneAgreedOnCount2(groupAnswer)
-    return allAg
+def main():
+    allGroupsAnswersFirstPart = getInputForFirstPart(INPUT_FILE)
+    print(getUniqueAnswersCountInAllGroups(allGroupsAnswersFirstPart))
+
+    allGroupsAnswersSecondPart = getInputForSecondPart(INPUT_FILE)
+    print(countCommonGroupAnswersInAllGroups(allGroupsAnswersSecondPart))
 
 
+class AnswersTester(unittest.TestCase):
+    def test_getUniqueAnswersCountInAllGroups_correntUniqueAnswersCountReturned(self):
+        allGroupsAnswers = getInputForFirstPart(TEST_INPUT)
+        self.assertEqual(11, getUniqueAnswersCountInAllGroups(allGroupsAnswers))
+
+    def test_countCommonGroupAnswersInAllGroups_correctCommonAnswersCountReturned(self):
+        allGroupsAnswers = getInputForSecondPart(TEST_INPUT)
+        self.assertEqual(6, countCommonGroupAnswersInAllGroups(allGroupsAnswers))
 
 
+if __name__ == '__main__':
+    # main()
+    unittest.main()
 
-
-
-
-
-
-v = getInput(TEST_INPUT)
-#print(getUniqueAnswersCountInAllGroups(v)) # 6416
-
-b = getInputSecondPart(TEST_INPUT)
-c = getInputSecondPart(INPUT_FILE)
-#print(b)
-#print(c)
-
-#print(getAnswersEveryoneAgreedOnCount(b))
-#print(getAnswersEveryoneAgreedOnCount(c)) # 3225 is too high 3128 too high  3050!!!!
-
-#print(getAnswersEveryoneAgreedOnCount2(c))
-print(getAll(c))
-
-
-#print(getAnswersCountEveryoneAgreedOn([c[0]]))
-#print(getAnswersCountEveryoneAgreedOn([['e', 'e', 'e', 'e']])) #1
-#print(getAnswersCountEveryoneAgreedOn([ ['pfyh', 'hyf', 'dhfy']])) #3
-#print(getAnswersCountEveryoneAgreedOn([['dqbpwhoar', 'pqohgd']])) #5
-#print(getAnswersCountEveryoneAgreedOn([['sakhxnu', 'cdowx', 'xlj', 'lnxh', 'njxhf']])) #1
+# I know it should not be here, just please have a look at it!
+# def getAnswersEveryoneAgreedOnCount(allGroups: List[List]):
+#     allAgreedAnswers = 0
+#     for groupAnswer in allGroups:
+#         shortestStringInGroupAnswer = min(groupAnswer, key=len)
+#         commonCharsNum = 0
+#         for char in shortestStringInGroupAnswer:
+#             isCharCommonInAllStrings = True
+#             for i in range(0, len(groupAnswer)):
+#                 if char not in groupAnswer[i]:
+#                     isCharCommonInAllStrings = False
+#                     break
+#             if isCharCommonInAllStrings:
+#                 commonCharsNum += 1
+#         allAgreedAnswers += commonCharsNum
+#
+#     return allAgreedAnswers
