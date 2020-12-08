701a68e2aa05ad2dfd4ba3db8a948479f349cc04
zseen <43151120+zseen@users.noreply.github.com>
2020-12-08 20:41:54 +0000

    Remove unnecessary variable and method

--- 2020/08/Day8-HandheldHalting.py
+++ 2020/08/Day8-HandheldHalting.py
@@ -22,30 +22,28 @@ def followInstructions(instructions: List):
     if not instructions:
         raise ValueError("No insstructions to follow")
     currentInstruction = instructions[0]
-    currentInstructionIndex = instructions.index(currentInstruction)
+
     while True:
         if currentInstruction.operation == Operation.NO_OPERATION.value:
             if currentInstruction.isFollowed:
                 break
-            prevInstruction = currentInstruction
+            currentInstruction.isFollowed = True
             currentInstructionIndex = instructions.index(currentInstruction)
-            currentInstruction = instructions[currentInstructionIndex + 1]
-            prevInstruction.isFollowed = True
+            currentInstruction = instructions[currentInstructionIndex % len(instructions) + 1]
         if currentInstruction.operation == Operation.ACCELERATE.value:
             if currentInstruction.isFollowed:
                 break
+            currentInstruction.isFollowed = True
             accelerator += int(currentInstruction.argument)
             currentInstructionIndex = instructions.index(currentInstruction)
-            prevInstruction = currentInstruction
-            currentInstruction = instructions[currentInstructionIndex + 1]
-            prevInstruction.isFollowed = True
+            currentInstruction = instructions[currentInstructionIndex % len(instructions) + 1]
         if currentInstruction.operation == Operation.JUMP.value:
             if currentInstruction.isFollowed:
                 break
+            currentInstruction.isFollowed = True
             currentInstructionIndex = instructions.index(currentInstruction)
-            prevInstruction = currentInstruction
-            currentInstruction = instructions[currentInstructionIndex + int(currentInstruction.argument)]
-            prevInstruction.isFollowed = True
+            currentInstruction = instructions[currentInstructionIndex % len(instructions) + int(currentInstruction.argument)]
+
     return accelerator
 
 
@@ -64,10 +62,6 @@ def getInput(inputFile):
     return instructionsLines
 
 
-def extendInstructions(instructions):
-    for i in range(10):
-        instructions.extend(instructions)
-    return instructions
 
 # l = getInput(TEST_INPUT_FILE)
 #
@@ -75,6 +69,5 @@ def extendInstructions(instructions):
 # print(accValue)
 
 instructions = getInput(INPUT_FILE)
-ins = extendInstructions(instructions)
-accValue = followInstructions(ins)  # not 87
+accValue = followInstructions(instructions)  # 1594
 print(accValue)
