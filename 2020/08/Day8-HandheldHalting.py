a531fd70e0eb7b8669b105dc5be6f8e5398b6417
zseen <43151120+zseen@users.noreply.github.com>
2020-12-09 00:00:29 +0000

    Rename methods

--- 2020/08/Day8-HandheldHalting.py
+++ 2020/08/Day8-HandheldHalting.py
@@ -1,12 +1,12 @@
-import  unittest
+import unittest
 from typing import List
 from enum import Enum
 from copy import deepcopy
 
 INPUT_FILE = "input.txt"
-TEST_INPUT_FILE = "test_input.txt"
-TEST_INPUT_FILE_SECOND_PART_FIRST = "test_input_second_part_1.txt"
-TEST_INPUT_FILE_SECOND_PART_SECOND = "test_input_second_part_2.txt"
+TEST_INPUT_FILE_LOOP = "test_input_loop.txt"
+TEST_INPUT_FILE_TERMINATION = "test_input_termination.txt"
+
 
 class Operation(Enum):
     JUMP = "jmp"
@@ -20,17 +20,15 @@ class Instruction:
         self.argument = argument
         self.isFollowed = False
 
-def followInstructions(instructions: List):
+
+def getAcceleratorCountInOneLoop(instructions: List):
     accelerator = 0
     if not instructions:
         raise ValueError("No instructions to follow")
     currentInstruction = instructions[0]
     nextInstructionOffset = 1
 
-    shouldTerminate = False
-
-    while not shouldTerminate:
-        shouldTerminate = currentInstruction == instructions[-1]
+    while True:
         if currentInstruction.isFollowed:
             break
 
@@ -39,33 +37,25 @@ def followInstructions(instructions: List):
             pass
         elif currentInstruction.operation == Operation.ACCELERATE.value:
             accelerator += int(currentInstruction.argument)
-
         elif currentInstruction.operation == Operation.JUMP.value:
             nextInstructionOffset = int(currentInstruction.argument)
-
         else:
             raise ValueError("Unexpected instruction")
 
         currentInstruction.isFollowed = True
-        currentInstruction = instructions[(currentInstructionIndex % (len(instructions) -1)) + nextInstructionOffset]
+        currentInstruction = instructions[(currentInstructionIndex % (len(instructions) - 1)) + nextInstructionOffset]
         nextInstructionOffset = 1
-
 
     return accelerator
 
 
-def getAcceleratorWithTermination(instructions: List):
-    # print("In getAcceleratorWithTermination()")
-    # for ins in instructions:
-    #     print(ins.operation,  end=' ')
-
+def getAcceleratorCountWithTermination(instructions: List[Instruction]):
     accelerator = 0
-    #print("-------")
     if not instructions:
         raise ValueError("No instructions to follow")
+
     currentInstruction = instructions[0]
     nextInstructionOffset = 1
-
     shouldTerminate = False
 
     while not shouldTerminate:
@@ -74,13 +64,11 @@ def getAcceleratorWithTermination(instructions: List):
         if currentInstruction.isFollowed:
             break
 
-
         currentInstructionIndex = instructions.index(currentInstruction)
         if currentInstruction.operation == Operation.NO_OPERATION.value:
             pass
         elif currentInstruction.operation == Operation.ACCELERATE.value:
             accelerator += int(currentInstruction.argument)
-
         elif currentInstruction.operation == Operation.JUMP.value:
             nextInstructionOffset = int(currentInstruction.argument)
 
@@ -88,43 +76,25 @@ def getAcceleratorWithTermination(instructions: List):
             raise ValueError("Unexpected instruction")
 
         currentInstruction.isFollowed = True
-        currentInstruction = instructions[(currentInstructionIndex % (len(instructions) -1)) + nextInstructionOffset]
+        currentInstruction = instructions[(currentInstructionIndex % (len(instructions) - 1)) + nextInstructionOffset]
         nextInstructionOffset = 1
-
-
 
     return accelerator, shouldTerminate
 
 
-def getAlternativeInstructionsRun(instructionLines):
-    originalInstructionsLine = deepcopy(instructionLines)
-    #originalInstructionsLine = (instructionLines).copy()
-    secndCopy = deepcopy(originalInstructionsLine)
-    #print(instructionLines)
+def getAcceleratorCountWithFixedInstructionProcess(instructions: List[Instruction]):
+    originalInstructions = deepcopy(instructions)
 
-    for i in range(0, len(instructionLines)):
-        # print("In getAlternativeInstructionsRun()")
-        # for ins in instructionLines:
-        #     print(ins.operation, end=" ")
-        # print("-----------------")
-        #if instructionLines[i].operation == Operation.JUMP.value or instructionLines[i].operation == Operation.NO_OPERATION.value:
-        if instructionLines[i].operation == Operation.JUMP.value:
-            instructionLines[i].operation = Operation.NO_OPERATION.value
-        elif instructionLines[i].operation == Operation.NO_OPERATION.value:
-            instructionLines[i].operation = Operation.JUMP.value
-        accWithTerm = getAcceleratorWithTermination(instructionLines)
+    for i in range(0, len(instructions)):
+        if instructions[i].operation == Operation.JUMP.value:
+            instructions[i].operation = Operation.NO_OPERATION.value
+        elif instructions[i].operation == Operation.NO_OPERATION.value:
+            instructions[i].operation = Operation.JUMP.value
+        accWithTerm = getAcceleratorCountWithTermination(instructions)
         if accWithTerm[1]:
             return accWithTerm[0]
-        instructionLines[i] = originalInstructionsLine[i]
-        for node in instructionLines:
-            node.isFollowed = False
 
-
-
-
-
-
-
+        instructions = deepcopy(originalInstructions)
 
 
 def getInput(inputFile):
@@ -140,25 +110,15 @@ def getInput(inputFile):
     return instructionsLines
 
 
+def main():
+    instructions = getInput(INPUT_FILE)
+    acceleratorCountInOneLoop = getAcceleratorCountInOneLoop(instructions)
+    print(acceleratorCountInOneLoop)
 
-#test_input_lines = getInput(TEST_INPUT_FILE)
-#accValue = followInstructions(l)
+    instructions = getInput(INPUT_FILE)
+    acceleratorIfTerminationFixed = getAcceleratorCountWithFixedInstructionProcess(instructions)
+    print(acceleratorIfTerminationFixed)
 
-instructions = getInput(INPUT_FILE)
-#accValue2 = followInstructions(instructions)  # 1594
 
-#ins = getInput(TEST_INPUT_FILE_SECOND_PART_SECOND)
-#acc = followInstructions(ins)
-#print(accValue == 5, accValue2 == 1594, acc == 8)
-
-#inst = getInput(TEST_INPUT_FILE_SECOND_PART_SECOND)
-#print(followInstructionsWithTermination(inst))
-
-linesNeedToBeCorrected = getInput(TEST_INPUT_FILE_SECOND_PART_FIRST)
-print(getAlternativeInstructionsRun(linesNeedToBeCorrected))
-
-#print(getAcceleratorWithTermination(ins))
-#print(getAcceleratorWithTermination(test_input_lines))
-
-#print(getAlternativeInstructionsRun(ins))
-print(getAlternativeInstructionsRun(instructions))
+if __name__ == '__main__':
+    main()
