cea1a685f832ed4d0f2d24500f98804b4ad3bc1e
zseen <43151120+zseen@users.noreply.github.com>
2020-12-09 20:01:58 +0000

    Use list comprehension when handling the input file

--- 2020/04/Day4-PassportProcessing.py
+++ 2020/04/Day4-PassportProcessing.py
@@ -8,12 +8,12 @@ TEST_INPUT_FILE_ONE = "test_input_part_1.txt"
 TEST_INPUT_FILE_TWO = "test_input_part_2.txt"
 
 PASSPORT_FIELDS_TO_VALID_REGEX_VALUES = {"byr": '19[2-9][0-9]|200[0-2]',
-                                              "iyr": '201[0-9]|2020',
-                                              "eyr": '202[0-9]|2030',
-                                              "hgt": '1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in',
-                                              "hcl": '#[0-9a-f]{6}',
-                                              "ecl": 'amb|blu|brn|gry|grn|hzl|oth',
-                                              "pid": '[0-9]{9}', "cid": '.*'}
+                                         "iyr": '201[0-9]|2020',
+                                         "eyr": '202[0-9]|2030',
+                                         "hgt": '1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in',
+                                         "hcl": '#[0-9a-f]{6}',
+                                         "ecl": 'amb|blu|brn|gry|grn|hzl|oth',
+                                         "pid": '[0-9]{9}', "cid": '.*'}
 
 COUNTRY_ID_FIELD = "cid"
 
@@ -22,14 +22,8 @@ def getInput(inputFile):
     with open(inputFile, "r") as inputFile:
         allDataForRawPassports = inputFile.read()
         allDataForRawPassports = allDataForRawPassports.split("\n\n")
-
-        allRawPassports = []
-        for rawPassports in allDataForRawPassports:
-            rawPassports = rawPassports.split()
-            allRawPassports.append(rawPassports)
-
+        allRawPassports = [rawPassport.split() for rawPassport in allDataForRawPassports]
         return getAllPassportsWithFieldToData(allRawPassports)
-
 
 
 def createPassportDict(rawPassport: List):
@@ -42,7 +36,7 @@ def createPassportDict(rawPassport: List):
 
 def getAllPassportsWithFieldToData(rawPassportsCollection: List):
     return [createPassportDict(rawPassport) for rawPassport in
-                     rawPassportsCollection]
+            rawPassportsCollection]
 
 
 def isPassportFormatValid(passport: dict):
