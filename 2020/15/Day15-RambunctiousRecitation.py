802d8d7d32d7c2e10c9f43ebbf72bf8c18b62e88
zseen <43151120+zseen@users.noreply.github.com>
2020-12-16 03:19:38 +0000

    Rename a method and variables

--- 2020/15/Day15-RambunctiousRecitation.py
+++ 2020/15/Day15-RambunctiousRecitation.py
@@ -5,40 +5,43 @@ from copy import deepcopy
 INPUT = [1, 20, 8, 12, 0, 14]
 TEST_INPUT = [0, 3, 6]
 
-UPPER_RANGE_FIRST_PART = 2020
-UPPER_RANGE_SECOND_PART = 30000000
+UPPER_BOUND_FIRST_PART = 2020
+UPPER_BOUND_SECOND_PART = 30000000
 
 
-def getUpperRangethNum(initialNums: List[int], upperRange: int):
+def getNthNum(initialNums: List[int], upperBound: int):
+    if not initialNums:
+        raise ValueError("No initial nums received")
+
     numsSaid = deepcopy(initialNums)
-    numsToTurnLastSaid = dict()
-    for i in range(len(numsSaid)):
-        numsToTurnLastSaid[numsSaid[i]] = i + 1
+    numToTurnLastSaid = dict()
+    for i in range(len(initialNums)):
+        numToTurnLastSaid[numsSaid[i]] = i + 1
 
-    for turnNum in range(len(numsSaid), upperRange):
+    for turnNum in range(len(numsSaid), upperBound):
         lastSaidNum = numsSaid[-1]
-        if lastSaidNum not in numsToTurnLastSaid:
+        if lastSaidNum not in numToTurnLastSaid:
             numsSaid.append(0)
         else:
-            lastTurnNumWasSaid = numsToTurnLastSaid[lastSaidNum]
+            lastTurnNumWasSaid = numToTurnLastSaid[lastSaidNum]
             numsSaid.append(turnNum - lastTurnNumWasSaid)
-        numsToTurnLastSaid[lastSaidNum] = turnNum
+        numToTurnLastSaid[lastSaidNum] = turnNum
 
     return numsSaid[-1]
 
 
 def main():
-    print(getUpperRangethNum(INPUT, UPPER_RANGE_FIRST_PART))  # 492
+    print(getNthNum(INPUT, UPPER_BOUND_FIRST_PART))  # 492
 
-    print(getUpperRangethNum(INPUT, UPPER_RANGE_SECOND_PART))  # 63644
+    print(getNthNum(INPUT, UPPER_BOUND_SECOND_PART))  # 63644
 
 
 class NumGameTester(unittest.TestCase):
-    def test_getUpperRangethNum_smallerRange_correctNumReturned(self):
-        self.assertEqual(436, getUpperRangethNum(TEST_INPUT, UPPER_RANGE_FIRST_PART))
+    def test_getNthNum_smallerUpperBound_correctNumReturned(self):
+        self.assertEqual(436, getNthNum(TEST_INPUT, UPPER_BOUND_FIRST_PART))
 
-    def test_getUpperRangethNum_largerRange_correctNumReturned(self):
-        self.assertEqual(175594, getUpperRangethNum(TEST_INPUT, UPPER_RANGE_SECOND_PART))
+    def test_getNthNum_largerUpperBound_correctNumReturned(self):
+        self.assertEqual(175594, getNthNum(TEST_INPUT, UPPER_BOUND_SECOND_PART))
 
 
 if __name__ == '__main__':
