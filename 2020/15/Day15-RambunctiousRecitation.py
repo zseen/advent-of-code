199c50dcd4625c071a835b73ed12d5617f4ee52f
zseen <43151120+zseen@users.noreply.github.com>
2020-12-16 00:20:57 +0000

    Add solution to the second part

--- 2020/15/Day15-RambunctiousRecitation.py
+++ 2020/15/Day15-RambunctiousRecitation.py
@@ -3,17 +3,19 @@ import unittest
 INPUT = [1,20,8,12,0,14]
 TEST_INPUT = [0, 3, 6]
 
+UPPER_RANGE_FIRST_PART = 2020
+UPPER_RANGE_SECOND_PART = 30000000
 
 
-def play(inputArray):
+def play(inputArray, upperRange):
     numsToSay = inputArray
     numsToTurn = dict()
     for i in range(len(inputArray)):
         numsToTurn[inputArray[i]] = i + 1
 
-    for i in range(len(inputArray)+1, 2021):
+    for i in range(len(inputArray)+1, upperRange + 1):
         lastSaidNum = numsToSay[-1]
-        if numsToSay.count(lastSaidNum) == 1:
+        if lastSaidNum not in numsToTurn:
             numsToSay.append(0)
         else:
             lastTurnNumWasSaid = numsToTurn[lastSaidNum]
@@ -25,28 +27,52 @@ def play(inputArray):
     return numsToSay
 
 
-numsTest1 = play(TEST_INPUT)
-print(numsTest1[-1])
+#numsTest1 = play(TEST_INPUT, UPPER_RANGE_FIRST_PART)
+#print(numsTest1[-1])
+#
+# numsTest2 = play([1,3,2], UPPER_RANGE_FIRST_PART)
+# print(numsTest2[-1])
+#
+# numsTest3 = play([2,1,3], UPPER_RANGE_FIRST_PART)
+# print(numsTest3[-1])
+#
+# numsTest4 = play([2,3,1], UPPER_RANGE_FIRST_PART)
+# print(numsTest4[-1])
+#
+# numsTest5 = play([3,2,1], UPPER_RANGE_FIRST_PART)
+# print(numsTest5[-1])
+#
+# numsTest6 = play([3,1,2], UPPER_RANGE_FIRST_PART)
+# print(numsTest6[-1])
 
-numsTest2 = play([1,3,2])
-print(numsTest2[-1])
 
-numsTest3 = play([2,1,3])
-print(numsTest3[-1])
 
-numsTest4 = play([2,3,1])
-print(numsTest4[-1])
 
-numsTest5 = play([3,2,1])
-print(numsTest5[-1])
-
-numsTest6 = play([3,1,2])
-print(numsTest6[-1])
 
 print("--------------------------------")
-numsMainRun = play(INPUT)
+numsMainRun = play(INPUT, UPPER_RANGE_FIRST_PART)
 print(numsMainRun[-1])
+print("--------------------------------")
+
+# numsTest1 = play(TEST_INPUT, UPPER_RANGE_SECOND_PART)
+# print(numsTest1[-1])
+#
+# numsTest2 = play([1,3,2], UPPER_RANGE_SECOND_PART)
+# print(numsTest2[-1])
+#
+# numsTest3 = play([2,1,3], UPPER_RANGE_SECOND_PART)
+# print(numsTest3[-1])
+#
+# numsTest4 = play([2,3,1], UPPER_RANGE_SECOND_PART)
+# print(numsTest4[-1])
+#
+# numsTest5 = play([3,2,1], UPPER_RANGE_SECOND_PART)
+# print(numsTest5[-1])
+#
+# numsTest6 = play([3,1,2], UPPER_RANGE_SECOND_PART)
+# print(numsTest6[-1])
 
 
 
-
+numsMainRun = play(INPUT, UPPER_RANGE_SECOND_PART)
+print(numsMainRun[-1])
