cde3bbefb598683911aab4dd69255546ff874625
zseen <43151120+zseen@users.noreply.github.com>
2021-01-11 21:58:36 +0000

    Start refactoring part one

--- 2020/19/Day19-MonsterMessages.py
+++ 2020/19/Day19-MonsterMessages.py
@@ -9,70 +9,62 @@ TEST_INPUT_FILE_TWO = "test_input_two.txt"
 
 def getInput(fileName: str):
     rules: Dict[int, List[str]] = {}
-    messages: List[str] = []
+
     with open(fileName, "r") as inputFile:
         lines = inputFile.read()
-        linesSplitByEmptyLine = lines.split("\n\n")
-        rulesRawSeparated = linesSplitByEmptyLine[0].split("\n")
+        linesChunks = lines.split("\n\n")
 
-        for line in rulesRawSeparated:
-            line = line.strip("\n")
-            lineSplit = line.split(": ")
-            rules[int(lineSplit[0])] = lineSplit[1].strip('""').split(" ")
+        rawRules = linesChunks[0].split("\n")
+        for rawRule in rawRules:
+            rawRuleSplit = rawRule.split(": ")
+            rules[int(rawRuleSplit[0])] = rawRuleSplit[1].strip('""').split(" ")
 
-        messagesSeparated = linesSplitByEmptyLine[1].split("\n")
-        for line in messagesSeparated:
-            messages.append(line)
-
+        messages= linesChunks[1].split("\n")
 
     return rules, messages
 
 
-# find the num witch char a and b first, add them to resolved
-# go through all others to see if any of them can be resolved (this case num 3 with values 4 and 5 will be resolvable)
-# check what can be resolved with this added num
-
 
 def resolve(rules):
-    resolvedNumToValues = dict()
+    resolvedNumToValues = initializeResolvedNumToValues(rules)
 
+    while len(resolvedNumToValues) != len(rules):
+        for key, values in rules.items():
+            if key not in resolvedNumToValues:
+                resolvedValue = createResolvedValueFromValues(values, resolvedNumToValues)
+                if resolvedValue:
+                    resolvedNumToValues[key] = "(" + resolvedValue + ")"
+
+    if 0 not in resolvedNumToValues:
+        raise ValueError("0 not in resolvedNumToValues.")
+
+    return resolvedNumToValues[0]
+
+def initializeResolvedNumToValues(rules):
+    resolvedNumToValues: Dict[int, str] = dict()
     for key, values in rules.items():
         if "a" in values:
             resolvedNumToValues[key] = "a"
         elif "b" in values:
             resolvedNumToValues[key] = "b"
-
-    #print(resolvedNumToValues)
-
+    return resolvedNumToValues
 
 
-    while len(resolvedNumToValues) != len(rules) - 1:
-        for key, values in rules.items():
-            isAllValueInResolved = True
-            if key not in resolvedNumToValues and key != 0:
-                resolvedValue = ""
-                if "|" in values:
-                    resolvedValue += "("
-                for value in values:
-                    if value.isnumeric() and int(value) not in resolvedNumToValues:
-                        isAllValueInResolved = False
-                    else:
-                        if value.isnumeric():
-                            resolvedValue += resolvedNumToValues[int(value)]
-                        elif value == "|":
-                            resolvedValue += "|"
-                if isAllValueInResolved:
-                    if "|" in values:
-                        resolvedValue += ")"
-                    resolvedNumToValues[key] = resolvedValue
+def createResolvedValueFromValues(values, resolvedNumToValues):
+    resolvedValue = ""
+    for value in values:
+        if value.isnumeric() and int(value) not in resolvedNumToValues:
+            return ""
+        resolvedValue += buildResolvedValue(value, resolvedNumToValues)
+    return resolvedValue
 
-    #print(resolvedNumToValues)
 
-    expressionForZero = ""
-    for value in rules[0]:
-        expressionForZero += "(" + resolvedNumToValues[int(value)] + ")"
 
-    return expressionForZero
+def buildResolvedValue(value, resolvedNumToValues):
+    if value.isnumeric():
+        return resolvedNumToValues[int(value)]
+    elif value == "|":
+        return "|"
 
 
 def getAllValidMessagesCount(messages: List[str], rules):
@@ -82,10 +74,7 @@ def getAllValidMessagesCount(messages: List[str], rules):
 
 def isMessageValid(wordCandidate: str, rules):
     patternForMessage = resolve(rules)
-    matching = re.fullmatch(patternForMessage, wordCandidate)
-    return matching is not None
-
-
+    return re.fullmatch(patternForMessage, wordCandidate)
 
 
 rules, messages = getInput(INPUT_FILE)
