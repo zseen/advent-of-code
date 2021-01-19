8d505a09bb3cc1eac0b9c78b7710c2e9652f6487
zseen <43151120+zseen@users.noreply.github.com>
2021-01-19 19:01:46 +0000

    Add unittests to both part and move methods into own class for the second part

--- 2020/19/Day19-MonsterMessages.py
+++ 2020/19/Day19-MonsterMessages.py
@@ -1,117 +1,100 @@
 import unittest
 from typing import List, Dict
 import re
-from copy import deepcopy
 
 INPUT_FILE = "input.txt"
 TEST_INPUT_FILE = "test_input.txt"
-TEST_INPUT_FILE_TWO = "test_input_two.txt"
-TEST_INPUT_FILE_THREE = "test_input_three.txt"
 
 RulesType = Dict[int, List[str]]
+RuleToDecipheredValueType = Dict[int, str]
 
 
-class ValidMessagesFinder:
+class ValidMessagesCounter:
     def __init__(self, rules: RulesType, messages: List[str]):
         self._rules = rules
         self._messages = messages
-        self._resolvedRuleNumToValues: Dict[int, str] = dict()
+        self._ruleNumToDecipheredValue: RuleToDecipheredValueType = dict()
 
-    def getResolvedRuleNumToValues(self):
-        return self._resolvedRuleNumToValues
-
-    def getAllValidMessagesCount(self) -> int:
-        self._resolveRules()
+    def getValidMessagesCount(self) -> int:
         return len([message for message in self._messages if self._isMessageValid(message)])
 
-    def _resolveRules(self) -> None:
-        self._resolvedRuleNumToValues = self._initializeResolvedNumToValues()
+    def decipherRules(self) -> None:
+        self._ruleNumToDecipheredValue: RuleToDecipheredValueType = self._initializeDecipheredRuleNumToValues()
 
-        while len(self._resolvedRuleNumToValues) != len(self._rules):
+        while len(self._ruleNumToDecipheredValue) != len(self._rules):
             for key, values in self._rules.items():
-                if key not in self._resolvedRuleNumToValues:
-                    resolvedValue = self._createResolvedValueFromValues(values)
-                    if resolvedValue:
-                        self._resolvedRuleNumToValues[key] = "(" + resolvedValue + ")"
+                if key not in self._ruleNumToDecipheredValue:
+                    decipheredValue: str = self._createDecipheredValueFromValues(values)
+                    if decipheredValue:
+                        self._ruleNumToDecipheredValue[key] = "(" + decipheredValue + ")"
 
-
-
-    def _initializeResolvedNumToValues(self) -> Dict[int, str]:
-        resolvedNumToValues: Dict[int, str] = dict()
+    def _initializeDecipheredRuleNumToValues(self) -> RuleToDecipheredValueType:
+        decipheredNumToValues: RuleToDecipheredValueType = dict()
         for key, value in self._rules.items():
             if "a" in value:
-                resolvedNumToValues[key] = "a"
+                decipheredNumToValues[key] = "a"
             elif "b" in value:
-                resolvedNumToValues[key] = "b"
-        return resolvedNumToValues
+                decipheredNumToValues[key] = "b"
 
+        return decipheredNumToValues
 
-    def _createResolvedValueFromValues(self, values: List[str]) -> str:
-        resolvedValue: str = ""
+    def _createDecipheredValueFromValues(self, values: List[str]) -> str:
+        decipheredValue: str = ""
         for value in values:
-            if value.isnumeric() and int(value) not in self._resolvedRuleNumToValues:
+            if value.isnumeric() and int(value) not in self._ruleNumToDecipheredValue:
                 return ""
-            resolvedValue += self._buildResolvedValue(value)
-        return resolvedValue
+            decipheredValue += self._buildDecipheredValue(value)
+        return decipheredValue
 
-
-    def _buildResolvedValue(self, value: str) -> str:
+    def _buildDecipheredValue(self, value: str) -> str:
         if value.isnumeric():
-            return self._resolvedRuleNumToValues[int(value)]
+            return self._ruleNumToDecipheredValue[int(value)]
         elif value == "|":
             return value
-        raise ValueError("Unexpected character + ", value)
+        raise ValueError("Unexpected character: ", value)
+
+    def _isMessageValid(self, message: str) -> re.Match:
+        if 0 not in self._ruleNumToDecipheredValue:
+            raise ValueError("0 not in ruleNumToDecipheredValue.")
+
+        return re.fullmatch(self._ruleNumToDecipheredValue[0], message)
 
 
-    def _isMessageValid(self, wordCandidate: str) -> re.Match:
-        if 0 not in self._resolvedRuleNumToValues :
-            raise ValueError("0 not in resolvedNumToValues.")
-
-        patternForMessage: str = self._resolvedRuleNumToValues[0]
-        return re.fullmatch(patternForMessage, wordCandidate)
-
-
-class ResolveLoopyRules(ValidMessagesFinder):
+class ValidMessagesCounterForLoopyRules(ValidMessagesCounter):
     def __init__(self, rules: RulesType, messages: List[str]):
         super().__init__(rules, messages)
-        #self.updatedRules = updatedRules
-        self.messages = messages
-        self._resolvedRuleNumToValues: Dict[int, str] = dict()
+        self._ruleNumToDecipheredValue: RuleToDecipheredValueType = dict()
 
+    def updateSpecificRules(self) -> None:
+        self.decipherRules()
 
-    def resolveUpdatedRules(self):
-        self._resolveRules()
+        if not 8 in self._ruleNumToDecipheredValue or not 11 in self._ruleNumToDecipheredValue or not 0 in self._ruleNumToDecipheredValue:
+            raise ValueError("8, 11, or 0 not in ruleNumToDecipheredValue.")
 
-    def getValidMessages(self):
-        self.resolveUpdatedRules()
-        validMessages = []
-        for message in self.messages:
-            if self.handleSpecial(message) or self._isMessageValid(message):
-                validMessages.append(message)
+        self._ruleNumToDecipheredValue[8] = "(" + self._ruleNumToDecipheredValue[42] + "+" + ")"
+        self._ruleNumToDecipheredValue[11] = "(" + self.getRegexBodyForEleven() + ")"
+        self._ruleNumToDecipheredValue[0] = self._ruleNumToDecipheredValue[8] + self._ruleNumToDecipheredValue[11]
 
-        return len(validMessages)
+    def getRegexBodyForEleven(self) -> str:
+        if not 42 in self._ruleNumToDecipheredValue or not 31 in self._ruleNumToDecipheredValue:
+            raise ValueError("42 or 31 not in ruleNumToDecipheredValue.")
 
-    def handleSpecial(self, currentMessage):
-        #8: 42 | 42 8
-        #11: 42 31 | 42 11 31
-        pass
+        regexBodyFor11 = ""
+        for i in range(1, 5):
+            regexBodyFor11 += self._ruleNumToDecipheredValue[42] + "{" + str(i) + "}" + self._ruleNumToDecipheredValue[31] + "{" + str(i) + "}" + "|"
 
+        # The last pipe is not needed in the regex
+        return regexBodyFor11[:-1]
 
-
-
-
-
-
-        
 
 def getInput(fileName: str) -> (RulesType, List[str]):
-    rules: RulesType= {}
+    rules: RulesType = dict()
 
     with open(fileName, "r") as inputFile:
         lines = inputFile.read()
         linesChunks = lines.split("\n\n")
 
-        rawRules = linesChunks[0].split("\n")
+        rawRules: List = linesChunks[0].split("\n")
         for rawRule in rawRules:
             rawRuleSplit = rawRule.split(": ")
             rules[int(rawRuleSplit[0])] = rawRuleSplit[1].strip('""').split(" ")
@@ -121,39 +104,32 @@ def getInput(fileName: str) -> (RulesType, List[str]):
     return rules, messages
 
 
-rules, messages = getInput(INPUT_FILE)
-validMessagesFinder = ValidMessagesFinder(rules, messages)
-print(validMessagesFinder.getAllValidMessagesCount())
+def main():
+    rules, messages = getInput(INPUT_FILE)
+
+    validMessagesCounter = ValidMessagesCounter(rules, messages)
+    validMessagesCounter.decipherRules()
+    print(validMessagesCounter.getValidMessagesCount())  # 187
+
+    validMessagesCounterForLoopyRules = ValidMessagesCounterForLoopyRules(rules, messages)
+    validMessagesCounterForLoopyRules.updateSpecificRules()
+    print(validMessagesCounterForLoopyRules.getValidMessagesCount())  # 392
 
 
+class ValidMessagesCounterTester(unittest.TestCase):
+    def test_getValidMessagesCount_noLoopInRules_correctCountReturned(self):
+        rules, messages = getInput(TEST_INPUT_FILE)
+        validMessagesCounter = ValidMessagesCounter(rules, messages)
+        validMessagesCounter.decipherRules()
+        self.assertEqual(validMessagesCounter.getValidMessagesCount(), 3)
 
-resolvedRuleNumToValues = validMessagesFinder.getResolvedRuleNumToValues()
-regexForEight = resolvedRuleNumToValues[8]
-regexForFortyTwo = resolvedRuleNumToValues[42]
-
-regexForModifiedEight = "(" + regexForFortyTwo + "+" + ")"
-
-regexForThirtyOne = resolvedRuleNumToValues[31]
-
-regexBodyForEleven = ""
-for i in range(1, 5):
-    regexBodyForEleven += regexForFortyTwo + "{" + str(i) + "}" + regexForThirtyOne + "{" + str(i) + "}" + "|"
-
-
-regexForModifiedEleven = "("  + regexBodyForEleven[:-1] + ")"
-regexModifiedForZero = regexForModifiedEight + regexForModifiedEleven
-
-cnt = 0
-for message in messages:
-    if re.fullmatch(regexModifiedForZero, message):
-        cnt += 1
-
-print(cnt)
+    def test_getValidMessagesCount_loopInRules_correctCountReturned(self):
+        rules, messages = getInput(TEST_INPUT_FILE)
+        validMessagesCounterForLoopyRules = ValidMessagesCounterForLoopyRules(rules, messages)
+        validMessagesCounterForLoopyRules.updateSpecificRules()
+        self.assertEqual(validMessagesCounterForLoopyRules.getValidMessagesCount(), 12)
 
 
-
-# rulesSecond, messagesSecond = getInput(TEST_INPUT_FILE_TWO)
-# rulesSecond[8] = ["42", "|", "42", "8"]
-# rulesSecond[11] = ["42", "31", "|", "42", "11", "31"]
-# vm = ResolveLoopyRules(rulesSecond, messagesSecond)
-# print(vm.getValidMessages())
+if __name__ == '__main__':
+    # main()
+    unittest.main()
